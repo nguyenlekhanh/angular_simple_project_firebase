c6fa292c9bbf45840af8988d6b4e13e0c23b8a88
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-09 11:30:07 -0600

    complex type, structure directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { TopHeaderComponent } from './top-header/top-header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    ProductListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
