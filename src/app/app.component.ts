c19aae740f3eb736a6103d14f16a9e3f787d86a1
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-08 23:27:07 -0600

    create topMenu MainMenu header using angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    TopMenuComponent,
+    MainMenuComponent
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
