845e98674c2375285782a451540cb7e28ed0a6f4
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-08 22:54:47 -0600

    create header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `<h3>Header</h3>`
+})
+export class HeaderComponent {
+  
+}
