b211520ce70119922e8b6a4bb6a7304f9e63a038
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-09 20:55:20 -0600

    Renderer2

--- src/app/CustomDirective/set-background.directive.ts
+++ src/app/CustomDirective/set-background.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appSetBackground]',
@@ -7,12 +7,19 @@ import { Directive, ElementRef } from '@angular/core';
 export class SetBackgroundDirective {
   //private element: ElementRef
   
-  constructor(private element: ElementRef) { 
+  constructor(private element: ElementRef, private renderer: Renderer2) { 
     //this.element = el;
   }
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#36454F';
-    this.element.nativeElement.style.color = 'white';
+    //access directly DOM
+    //this.element.nativeElement.style.backgroundColor = '#36454F';
+    //this.element.nativeElement.style.color = 'white';
+
+    //not directly access the DOM
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
+    this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    // this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example title');
   }
+
 }
