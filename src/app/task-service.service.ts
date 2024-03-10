7c69211ecbb67df181eba78baae27e0e25d7e793
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-10 15:38:22 -0500

    Subjects in RxJS: replace eventEmitter to Subject() Observable

--- src/app/task-service.service.ts
+++ src/app/task-service.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,10 @@ export class TaskServiceService {
 
   constructor() { }
 
-  CreateTask: EventEmitter<string> = new EventEmitter<string>();
+  //CreateTask: EventEmitter<string> = new EventEmitter<string>();
+  CreateTask = new Subject<string>();
 
   OnCreateTask(value: string) {
-    this.CreateTask.emit(value);
+    this.CreateTask.next(value);
   }
 }
