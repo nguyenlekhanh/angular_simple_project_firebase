5acee60b4272457ee7994660bdcf60ffa1e881f2
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-09 21:11:47 -0600

    @HostBinding Directive

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -8,13 +8,16 @@ import { ProductListComponent } from '../product-list/product-list.component';
 
 import { SetBackgroundDirective } from '../../CustomDirective/set-background.directive';
 
+import { AppHoverDirective } from '../../CustomDirective/app-hover.directive';
+
 @Component({
   selector: 'product-detail',
   standalone: true,
   imports: [
     CommonModule,
     FormsModule,
-    SetBackgroundDirective
+    SetBackgroundDirective,
+    AppHoverDirective
   ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
