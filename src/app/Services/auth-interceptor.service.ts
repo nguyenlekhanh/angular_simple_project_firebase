9dbdf0771a26bc850c3767930c904e24ad64729b
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-12 21:06:54 -0500

    Modify Request & Response using Interceptor

--- src/app/Services/auth-interceptor.service.ts
+++ src/app/Services/auth-interceptor.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@angular/core";
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { HttpInterceptorFn } from '@angular/common/http';
+import { tap } from "rxjs";
 
 export const authInterceptorServiceInterceptor: HttpInterceptorFn = (req, next) => {
+    const modifiedReq = req.clone({headers: req.headers.append('auth', 'abcccc')});
   console.log("Auth Interceptor called!");
-  return next(req);
+  return next(modifiedReq).pipe(
+    tap((event) => {
+        if(event.type === HttpEventType.Response) {
+            console.log('Response has arrived. Response data: ');
+            console.log(event.body);
+        }
+    })
+  );
 };
 
 // export class AuthInterceptorService implements HttpInterceptor{
